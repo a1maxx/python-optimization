560787a29cc2216834810f5119a53afb7ad1372e
a1maxx <yvzabdurrahman@gmail.com>
2022-05-29 23:42:36 -0400

    Adds from Mac

--- DroopAdmittance.py
+++ DroopAdmittance.py
@@ -1,5 +1,6 @@
+import math
+
 import pyomo.environ as pyo
-import cmath
 
 model = pyo.AbstractModel()
 model.N = pyo.Param(default=3)
@@ -20,67 +21,27 @@ model.SGmax = pyo.Param(model.B, initialize=1.0)
 model.R = pyo.Param(model.J, model.J)
 model.X = pyo.Param(model.J, model.J)
 
-model.ql = pyo.Var(model.J, initialize=0, within=pyo.Reals)
-model.pl = pyo.Var(model.J, initialize=0, within=pyo.Reals)
-model.pg = pyo.Var(model.J, initialize=0, within=pyo.Reals)
-model.qg = pyo.Var(model.J, initialize=0, within=pyo.Reals)
+model.ql = pyo.Var(model.J, initialize=0, within=pyo.NonNegativeReals)
+model.pl = pyo.Var(model.J, initialize=0, within=pyo.NonNegativeReals)
+model.pg = pyo.Var(model.J, initialize=0, within=pyo.NonNegativeReals)
+model.qg = pyo.Var(model.J, initialize=0, within=pyo.NonNegativeReals)
 model.y = pyo.Var(model.J, model.J)
 model.t = pyo.Var(model.J, model.J)
 
-model.v = pyo.Var(model.J, domain=pyo.Reals, initialize=1.0)
+model.v = pyo.Var(model.J, domain=pyo.NonNegativeReals, initialize=1.0)
 model.d = pyo.Var(model.J, domain=pyo.Reals, initialize=0)
 
-model.mp = pyo.Var(model.B, domain=pyo.Reals, initialize=0.5, bounds=(0, 1))
-model.nq = pyo.Var(model.B, domain=pyo.Reals, initialize=0.5, bounds=(0, 1))
+model.mp = pyo.Var(model.B, domain=pyo.NonNegativeReals, initialize=0.5, bounds=(0, 1))
+model.nq = pyo.Var(model.B, domain=pyo.NonNegativeReals, initialize=0.5, bounds=(0, 1))
 
 model.w = pyo.Var(domain=pyo.PositiveReals)
 
-
-# data.load(filename="C:\\Users\\Administrator\\Py Files\\model.dat", model=model)
-# data = pyo.DataPortal()
-# data.load(filename="/Users/my_mac/PycharmProjects/python-optimization/model2.dat", model=model)
-# model2 = model.create_instance(data)
-# model2.pprint()
 
 def obj_expression(m):
     return sum(abs(m.v[i] - 1.0) for i in m.J)
 
 
 model.o = pyo.Objective(rule=obj_expression, sense=pyo.minimize)
-
-
-def ax_constraint_rule3(m, i):
-    # return the expression for the constraint for i
-    return (m.pg[i] - m.pl[i]) - sum(
-        m.v[i] * m.v[j] * m.y[i, j] * pyo.cos(m.t[i, j] + m.d[j] - m.d[i]) for j in m.J) == 0
-
-
-def ax_constraint_rule4(m, i):
-    # return the expression for the constraint for i
-    return (m.qg[i] - m.ql[i]) + sum(
-        m.v[i] * m.v[j] * m.y[i, j] * pyo.sin(m.t[i, j] + m.d[j] - m.d[i]) for j in m.J) == 0
-
-
-def ax_constraint_rule5(m, i):
-    if i == 1:
-        return m.pg[i] == (1 / m.mp[i]) * (m.w0 - m.w)
-    else:
-        m.pg[i] == 0
-
-
-def ax_constraint_rule6(m, i):
-    if i == 1:
-        return m.qg[i] == (1 / m.nq[i]) * (m.V0 - m.v[i])
-    else:
-        m.pg[i] == 0
-
-
-# Frequency & voltage dependent load constraints
-def ax_constraint_rule5(m, i):
-    return m.pl[i] == m.p0[i] * pow(m.v[i] / m.V0, m.alpha) * (1 + m.KPF * (m.w - m.w0))
-
-def ax_constraint_rule6(m, i):
-    return m.ql[i] == m.q0[i] * pow(m.v[i] / m.V0, m.beta) * (1 + m.KQF * (m.w - m.w0))
 
 
 def admittance_rule(m, i, j):
@@ -112,19 +73,12 @@ model.cons13 = pyo.Constraint(model.J, model.J, rule=admittance_rule)
 model.cons14 = pyo.Constraint(model.J, model.J, rule=admittance_rule2)
 model.cons15 = pyo.Constraint(model.J, model.J, rule=admittance_rule3)
 
-# model.cons1 = pyo.Constraint(model.J, rule=ax_constraint_rule)
-# model.cons2 = pyo.Constraint(model.J, rule=ax_constraint_rule2)
-
-model.cons3 = pyo.Constraint(model.B, rule=ax_constraint_rule3)
-model.cons4 = pyo.Constraint(model.B, rule=ax_constraint_rule4)
-model.cons5 = pyo.Constraint(model.J, rule=ax_constraint_rule5)
-model.cons6 = pyo.Constraint(model.J, rule=ax_constraint_rule6)
-
-
 model.name = "DroopControlledIMG"
 opt = pyo.SolverFactory("ipopt")
 # instance = model.create_instance(filename=("C:\\Users\\Administrator\\Py Files\\model.dat"))
 instance = model.create_instance(filename="/Users/my_mac/PycharmProjects/python-optimization/model2.dat")
+instance.pprint()
+
 opt.options['max_iter'] = 50000
 opt.options['ma27_pivtol'] = 1e-3
 results = opt.solve(instance, tee=True)
---
+++ IPOPToy.py
@@ -0,0 +1,49 @@
+import math
+
+import pyomo.environ as pyo
+
+model = pyo.ConcreteModel()
+
+model.x1 = pyo.Var(within=pyo.NonNegativeReals, bounds=(1, 100))
+model.x2 = pyo.Var(within=pyo.NonNegativeReals, bounds=(1, 100))
+
+model.b = pyo.Param(initialize=10)
+
+
+def obj_func(m):
+    return pyo.sin(m.x1 * m.x2)
+
+
+model.o = pyo.Objective(rule=obj_func, sense=pyo.minimize)
+
+
+def constraint1(m):
+    return m.x1 + m.x2 >= m.x1 * m.x2 * pyo.cos(m.x2)
+
+
+model.cons1 = pyo.Constraint(rule=constraint1)
+
+opt = pyo.SolverFactory("ipopt")
+opt.options['max_iter'] = 50000
+opt.options['ma27_pivtol'] = 1e-10
+results = opt.solve(model, tee=True)
+
+for parmobject in model.component_objects(pyo.Param, active=True):
+    nametoprint = str(str(parmobject.name))
+    print("Parameter ", nametoprint)
+    for index in parmobject:
+        vtoprint = pyo.value(parmobject[index])
+        print("   ", index, vtoprint)
+
+for parmobject in model.component_objects(pyo.Var, active=True):
+    nametoprint = str(str(parmobject.name))
+    print("Variable ", nametoprint)
+    for index in parmobject:
+        vtoprint = pyo.value(parmobject[index])
+        print("   ", index, vtoprint)
+
+x1 = pyo.value(model.x1)
+x2 = pyo.value(model.x2)
+obj = math.sin(x1 * x2)
+obj
+x1 + x2 >= x1 * x2
